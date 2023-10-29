02ab93a2a9980fb0c3b5010025c552c059b400a3
Ivan Gabrusevich <ivan@gabrusevich.ru>
2023-10-29 14:57:11 +0300

    refactor: update styles and header

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import Lottie from 'react-lottie'; 
-import { useState } from 'react'
+import { useState } from 'react';
 import './Content.css';
 import animationData from '../../assets/web-designer-with-idea.json'; 
+import firstLogo from '../../assets/smartmontools.png';
 
 function Content() {
   const defaultOptions = {
@@ -11,7 +12,7 @@ function Content() {
     animationData: animationData,
   };
 
-  const [toggle, setToggle] = useState(1)
+  const [toggle, setToggle] = useState(0)
 
   function updateToggle(id) {
     setToggle(id)
@@ -19,8 +20,63 @@ function Content() {
 
   return (
     <div className="container">
+        <div className={toggle === 0 ? "content__show" : "content"}>
+            <Lottie options={defaultOptions} height={600} width={600} />
+        </div>
         <div className={toggle === 1 ? "content__show" : "content"}>
-            <Lottie options={defaultOptions} height={700} width={700} />
+        <img className="content__logo" src={firstLogo} width="250px" alt="firstLogo" />
+        <h1 className="content__header">Smartmontools</h1>
+        <p>
+        Smartmontools - это набор командных инструментов для мониторинга и управления
+        данными Self-Monitoring, Analysis and Reporting Technology (SMART) на ваших жестких дисках
+        и твердотельных накопителях. Эти инструменты позволяют вам проверить состояние и работоспособность
+        ваших накопителей и предоставляют информацию о состоянии жестких дисков и накопителей SSD.
+        Smartmontools - это набор командных инструментов для мониторинга и управления
+        данными Self-Monitoring, Analysis and Reporting Technology (SMART) на ваших жестких дисках
+        и твердотельных накопителях. Эти инструменты позволяют вам проверить состояние и работоспособность
+        ваших накопителей и предоставляют информацию о состоянии жестких дисков и накопителей SSD.
+        Smartmontools - это набор командных инструментов для мониторинга и управления
+        данными Self-Monitoring, Analysis and Reporting Technology (SMART) на ваших жестких дисках
+        и твердотельных накопителях. Эти инструменты позволяют вам проверить состояние и работоспособность
+        ваших накопителей и предоставляют информацию о состоянии жестких дисков и накопителей SSD.
+        Smartmontools - это набор командных инструментов для мониторинга и управления
+        данными Self-Monitoring, Analysis and Reporting Technology (SMART) на ваших жестких дисках
+        и твердотельных накопителях. Эти инструменты позволяют вам проверить состояние и работоспособность
+        ваших накопителей и предоставляют информацию о состоянии жестких дисков и накопителей SSD.
+        Smartmontools - это набор командных инструментов для мониторинга и управления
+        данными Self-Monitoring, Analysis and Reporting Technology (SMART) на ваших жестких дисках
+        и твердотельных накопителях. Эти инструменты позволяют вам проверить состояние и работоспособность
+        ваших накопителей и предоставляют информацию о состоянии жестких дисков и накопителей SSD.
+        Smartmontools - это набор командных инструментов для мониторинга и управления
+        данными Self-Monitoring, Analysis and Reporting Technology (SMART) на ваших жестких дисках
+        и твердотельных накопителях. Эти инструменты позволяют вам проверить состояние и работоспособность
+        ваших накопителей и предоставляют информацию о состоянии жестких дисков и накопителей SSD.
+
+        Smartmontools - это набор командных инструментов для мониторинга и управления
+        данными Self-Monitoring, Analysis and Reporting Technology (SMART) на ваших жестких дисках
+        и твердотельных накопителях. Эти инструменты позволяют вам проверить состояние и работоспособность
+        ваших накопителей и предоставляют информацию о состоянии жестких дисков и накопителей SSD.
+        Smartmontools - это набор командных инструментов для мониторинга и управления
+        данными Self-Monitoring, Analysis and Reporting Technology (SMART) на ваших жестких дисках
+        и твердотельных накопителях. Эти инструменты позволяют вам проверить состояние и работоспособность
+        ваших накопителей и предоставляют информацию о состоянии жестких дисков и накопителей SSD.
+        Smartmontools - это набор командных инструментов для мониторинга и управления
+        данными Self-Monitoring, Analysis and Reporting Technology (SMART) на ваших жестких дисках
+        и твердотельных накопителях. Эти инструменты позволяют вам проверить состояние и работоспособность
+        ваших накопителей и предоставляют информацию о состоянии жестких дисков и накопителей SSD.
+        Smartmontools - это набор командных инструментов для мониторинга и управления
+        данными Self-Monitoring, Analysis and Reporting Technology (SMART) на ваших жестких дисках
+        и твердотельных накопителях. Эти инструменты позволяют вам проверить состояние и работоспособность
+        ваших накопителей и предоставляют информацию о состоянии жестких дисков и накопителей SSD.
+        Smartmontools - это набор командных инструментов для мониторинга и управления
+        данными Self-Monitoring, Analysis and Reporting Technology (SMART) на ваших жестких дисках
+        и твердотельных накопителях. Эти инструменты позволяют вам проверить состояние и работоспособность
+        ваших накопителей и предоставляют информацию о состоянии жестких дисков и накопителей SSD.
+        Smartmontools - это набор командных инструментов для мониторинга и управления
+        данными Self-Monitoring, Analysis and Reporting Technology (SMART) на ваших жестких дисках
+        и твердотельных накопителях. Эти инструменты позволяют вам проверить состояние и работоспособность
+        ваших накопителей и предоставляют информацию о состоянии жестких дисков и накопителей SSD.
+        </p>
         </div>
         <div className={toggle === 2 ? "content__show" : "content"}>
             
